7a57aa98249449386cd8d2ad878db338cb629bba
Lenovo <Lenovo@192.168.0.10>
2019-03-01 14:32:45 +0530

    merge

---
+++ IOC11_Collection_Merge/src/com/nt/bean/EngCollege.java
@@ -0,0 +1,20 @@
+package com.nt.bean;
+
+import java.util.Set;
+
+public class EngCollege {
+
+    Set<String>subjects;
+
+	public void setSubjects(Set<String> subjects) {
+		this.subjects = subjects;
+	}
+
+	@Override
+	public String toString() {
+		return "User11 [subjects=" + subjects + "]";
+	}
+    
+    
+
+}
