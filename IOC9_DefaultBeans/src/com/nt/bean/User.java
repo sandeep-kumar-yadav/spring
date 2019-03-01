d2dab3277eaa7d8502dc1d60e12f57decd3a76ac
Lenovo <Lenovo@192.168.0.10>
2019-03-01 14:35:13 +0530

    default

---
+++ IOC9_DefaultBeans/src/com/nt/bean/User.java
@@ -0,0 +1,27 @@
+package com.nt.bean;
+
+public class User {
+
+     private int a;
+     
+
+	public int getA() {
+		return a;
+	}
+
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+
+	@Override
+	public String toString() {
+		return "B [a=" + a + "]";
+	}
+
+
+	
+	
+	
+}
